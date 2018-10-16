6821926402f50e5050269ec44b36ce5f2bd4e6d1
jfRenauld <jf.renauld@synakene.fr>
2018-10-16 15:05:20 +0200

    Added stairs interaction (slopy platforms)

--- js/scrollity.js
+++ js/scrollity.js
@@ -1,14 +1,18 @@
-// OBJECT MODELS //
+// --- OBJECT MODELS --- //
+
+// Player class - node of this file, handles all interactions between player and the rest of the game objects
 class Player {
   constructor(sprite, rectangle) {
     this.sprite = sprite;
     this.rectangle = rectangle;
     
+    // handle player horizontal speed
     this.velocityX = 0;
     this.maximumVelocityX = 15;
     this.accelerationX = 1.2;
     this.frictionX = 0.9;
     
+    // handle player vertical speed (acceleration/ gravity/ jump included)
     this.velocityY = 0;
     this.maximumVelocityY = 30;
     this.accelerationY = 3;
@@ -16,20 +20,21 @@ class Player {
 
     this.climbingSpeed = 5;
     
-    // boolean values to check whether the player falls or if he collides with a ladder
+    // boolean values to check whether the player falls, collides with a slope or if he collides with a ladder
     this.isOnGround = false;
     this.isOnLadder = false;
+    this.isOnSlope = false;
   }
 
   animate(state) {
-    if (state.keys[37] || state.keys[81]) { // left
+    if (state.keys[37] || state.keys[81]) { // left pressed
       this.velocityX = Math.max(
         this.velocityX - this.accelerationX,
         this.maximumVelocityX * -1,
       );
     }
 
-    if (state.keys[39] || state.keys[68]) { // right
+    if (state.keys[39] || state.keys[68]) { // right pressed 
       this.velocityX = Math.min(
         this.velocityX + this.accelerationX,
         this.maximumVelocityX,
@@ -57,6 +62,47 @@ class Player {
           me.y < you.y + you.height &&
           me.y + me.height > you.y) {
 
+        if (object.constructor.name === "LeftSlope") {
+          const meCenter = Math.round(me.x + (me.width / 2));
+          const youRight = you.x + you.width;
+          const youBottom = you.y + you.height;
+          const highest = you.y - me.height;
+          const lowest = youBottom - me.height;
+
+          this.isOnGround = true;
+          this.isOnSlope = true;
+
+          me.y = lowest - (meCenter - you.x);
+          me.y = Math.max(me.y, highest);
+          me.y = Math.min(me.y, lowest);
+
+          if (me.y >= lowest || me.y <= highest) {
+            this.isOnSlope = false;
+          }
+
+          return;
+        }
+
+        if (object.constructor.name === "RightSlope") {
+          const meCenter = Math.round(me.x + (me.width / 2));
+          const youBottom = you.y + you.height;
+          const highest = you.y - you.height;
+          const lowest = youBottom - me.height;
+
+          this.isOnGround = true;
+          this.isOnSlope = true;
+
+          me.y = highest + (meCenter - you.x);
+          me.y = Math.max(me.y, highest);
+          me.y = Math.min(me.y, lowest);
+
+          if (me.y >= lowest || me.y <= highest) {
+            this.isOnSlope = false;
+          }
+
+          return;
+        }
+
         if (object.constructor.name === "Ladder") {
           if (state.keys[38] || state.keys[40]) {
             this.isOnLadder = true;
@@ -77,6 +123,8 @@ class Player {
           if (me.y <= you.x - me.height) {
             this.isOnLadder = false;
           }
+
+          return;
         }
 
         if (collides && this.velocityY > 0 && you.y >= me.y) {
@@ -102,9 +150,10 @@ class Player {
       }
     })
 
-    if (state.keys[32] && this.isOnGround) {
+    if (state.keys[32] && this.isOnGround) { // Press spacebar to jump
         this.velocityY = this.jumpVelocity;
         this.isOnGround = false;
+        this.isOnSlope = false;
     }
 
     this.rectangle.x += this.velocityX;
@@ -150,6 +199,55 @@ class Ladder {
   }
 }
 
+class LeftSlope {
+  constructor(sprite, rectangle) {
+    this.sprite = sprite;
+    this.rectangle = rectangle;
+  }
+
+  get collides() {
+    return false;
+  }
+
+  animate(state) {
+    this.sprite.x = this.rectangle.x;
+    this.sprite.y = this.rectangle.y;
+  }
+}
+
+class RightSlope {
+  constructor(sprite, rectangle) {
+    this.sprite = sprite;
+    this.rectangle = rectangle;
+  }
+
+  get collides() {
+    return false;
+  }
+
+  animate(state) {
+    this.sprite.x = this.rectangle.x;
+    this.sprite.y = this.rectangle.y;
+  }
+}
+
+class Decal {
+  constructor(sprite, rectangle) {
+    this.sprite = sprite;
+    this.rectangle = rectangle;
+  }
+
+  get collides() {
+    return false;
+  }
+
+  animate(state) {
+    this.sprite.x = this.rectangle.x;
+    this.sprite.y = this.rectangle.y;
+  }
+}
+
+// Game class & functions used to instantiate the game scene and PixiJS concepts 
 class Game {
   constructor() {
     this.state = {
@@ -258,6 +356,8 @@ class Game {
   }
 }
 
+// --- GAME SCENE SETUP --- //
+
 // Create new instance of the game
 const game = new Game();
 
@@ -276,6 +376,7 @@ game.addObject(
   )
 );
 
+// Setup the different game objects and obstacles
 game.addObject(
   new Box(
     new PIXI.Sprite.fromImage(
@@ -316,6 +417,63 @@ game.addObject(
   )
 );
 
+game.addObject(
+  new LeftSlope(
+    new PIXI.Sprite.fromImage(
+      "https://s3.eu-west-3.amazonaws.com/scrollity-training/slope-left.png",
+    ),
+    new PIXI.Rectangle(
+      0 + 250,
+      window.innerHeight - 64 - 64 + 1,
+      64,
+      64,
+    ),
+  )
+);
+
+game.addObject(
+  new RightSlope(
+    new PIXI.Sprite.fromImage(
+      "https://s3.eu-west-3.amazonaws.com/scrollity-training/slope-right.png",
+    ),
+    new PIXI.Rectangle(
+      0 + 250 + 64 + 128,
+      window.innerHeight - 64 - 64 + 1,
+      64,
+      64,
+    ),
+  )
+);
+
+game.addObject(
+  new Decal(
+    new PIXI.Sprite.fromImage(
+      "https://s3.eu-west-3.amazonaws.com/scrollity-training/hill-base.png",
+    ),
+    new PIXI.Rectangle(
+      0 + 250,
+      window.innerHeight - 64 + 1,
+      128,
+      64,
+    ),
+  )
+);
+
+game.addObject(
+  new Box(
+    new PIXI.Sprite.fromImage(
+      "https://s3.eu-west-3.amazonaws.com/scrollity-training/hill-top.png",
+    ),
+    new PIXI.Rectangle(
+      0 + 250 + 64,
+      window.innerHeight - 64 - 64 + 1,
+      128,
+      64,
+    ),
+  )
+);
+
+// Instantiate the player character
 game.addObject(
   new Player(
     new PIXI.Sprite.fromImage("https://s3.eu-west-3.amazonaws.com/scrollity-training/player-idle.png"),
